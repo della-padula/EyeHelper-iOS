9766bac08aed902f5b4f0da5a8ab3675b79e7ac1
denny.k <denny.k@kakaocorp.com>
2020-05-29 19:26:36 +0900

    first commit

---
+++ EyeHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeHelper
+//
+//  Created by 김태인 on 2020/05/29.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# EyeHelper-iOS
